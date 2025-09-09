c44b18919cd91a481b385885cab8351e201e9937
Frank Yang <167046860+frankiscuwu@users.noreply.github.com>
2025-09-09 16:09:37 -0400

    Revert "completed exercise 1 and exercise 2: Added subtraction and dicision, …"

--- polynomial.py
+++ polynomial.py
@@ -6,10 +6,14 @@ class X:
         return "X"
 
     def evaluate(self, x_value):
-        return Int(x_value)
+        # TODO: Implement evaluation for variable X
+        # Should return an Int object with the given x_value
+        pass
 
     def simplify(self):
-        return self
+        # TODO (Optional Exercise): Implement simplification
+        # X cannot be simplified further, so return self
+        pass
 
 
 class Int:
@@ -20,10 +24,14 @@ class Int:
         return str(self.i)
 
     def evaluate(self, x_value):
-        return Int(self.i)
+        # TODO: Implement evaluation for integer constant
+        # Should return an Int object with the stored integer value
+        pass
 
     def simplify(self):
-        return self
+        # TODO (Optional Exercise): Implement simplification
+        # Integer constants cannot be simplified further, so return self
+        pass
 
 
 class Add:
@@ -35,7 +43,9 @@ class Add:
         return repr(self.p1) + " + " + repr(self.p2)
 
     def evaluate(self, x_value):
-        return Int(self.p1.evaluate(x_value).i + self.p2.evaluate(x_value).i)
+        # TODO: Implement evaluation for addition
+        # Should evaluate both operands and return their sum
+        pass
 
     def simplify(self):
         # TODO (Optional Exercise): Implement simplification
@@ -59,7 +69,9 @@ class Mul:
         return repr(self.p1) + " * " + repr(self.p2)
 
     def evaluate(self, x_value):
-        return Int(self.p1.evaluate(x_value).i * self.p2.evaluate(x_value).i)
+        # TODO: Implement evaluation for multiplication
+        # Should evaluate both operands and return their product
+        pass
 
     def simplify(self):
         # TODO (Optional Exercise): Implement simplification
@@ -74,16 +86,15 @@ class Sub:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) - " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " - " + repr(self.p2)
+        # TODO: Implement string representation for subtraction
+        # Should handle parentheses similar to Mul class
+        # Hint: Look at how Mul class handles parentheses
+        pass
 
     def evaluate(self, x_value):
-        return Int(self.p1.evaluate(x_value).i - self.p2.evaluate(x_value).i)
+        # TODO: Implement evaluation for subtraction
+        # Should return the difference of the two operands
+        pass
 
     def simplify(self):
         # TODO (Optional Exercise): Implement simplification
@@ -98,16 +109,15 @@ class Div:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
-            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
-                return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
-        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
-            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " / " + repr(self.p2)
+        # TODO: Implement string representation for division
+        # Should handle parentheses similar to Mul class
+        # Hint: Look at how Mul class handles parentheses
+        pass
 
     def evaluate(self, x_value):
-        return Int(self.p1.evaluate(x_value).i / self.p2.evaluate(x_value).i)
+        # TODO: Implement evaluation for division
+        # Should return the quotient of the two operands (use integer division //)
+        pass
 
     def simplify(self):
         # TODO (Optional Exercise): Implement simplification
@@ -117,8 +127,7 @@ class Div:
 
 
 # Original polynomial example
-poly = Add(Add(Int(4), Int(3)), Add(
-    X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 print("Original polynomial:", poly)
 
 # Test new Sub and Div classes (will fail until implemented)
